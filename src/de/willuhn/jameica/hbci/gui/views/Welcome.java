c3c3c341dae079c7f3dfe4cbce8cfa6bcc428e6e
willuhn <willuhn>
2004-02-09 13:06:03 +0000

    @C misc

---
+++ src/de/willuhn/jameica/hbci/gui/views/Welcome.java
@@ -0,0 +1,57 @@
+/**********************************************************************
+ * $Source$
+ * $Revision$
+ * $Date$
+ * $Author$
+ * $Locker$
+ * $State$
+ *
+ * Copyright (c) by willuhn.webdesign
+ * All rights reserved
+ *
+ **********************************************************************/
+
+package de.willuhn.jameica.hbci.gui.views;
+
+import org.eclipse.swt.widgets.Composite;
+
+import de.willuhn.jameica.gui.views.AbstractView;
+import de.willuhn.util.ApplicationException;
+
+/**
+ * 
+ */
+public class Welcome extends AbstractView
+{
+
+  /**
+   * @param parent
+   */
+  public Welcome(Composite parent)
+  {
+    super(parent);
+  }
+
+  /**
+   * @see de.willuhn.jameica.gui.views.AbstractView#bind()
+   */
+  public void bind() throws Exception
+  {
+  }
+
+  /**
+   * @see de.willuhn.jameica.gui.views.AbstractView#unbind()
+   */
+  public void unbind() throws ApplicationException
+  {
+  }
+
+}
+
+
+/**********************************************************************
+ * $Log$
+ * Revision 1.1  2004-02-09 13:06:03  willuhn
+ * @C misc
+ *
+ **********************************************************************/
