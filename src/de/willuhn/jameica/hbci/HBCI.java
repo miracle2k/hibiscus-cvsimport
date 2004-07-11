514785db07424f122af1c4f590d434f9c787241a
willuhn <willuhn>
2004-07-11 16:14:29 +0000

    @N erster Code fuer Dauerauftraege

--- src/de/willuhn/jameica/hbci/HBCI.java
+++ src/de/willuhn/jameica/hbci/HBCI.java
@@ -138,6 +138,17 @@ public class HBCI extends AbstractPlugin
    */
   public void update(double oldVersion) throws ApplicationException
   {
+  	if (oldVersion == 1.0)
+  	{
+			try {
+				EmbeddedDatabase db = getResources().getDatabase();
+				db.executeSQLScript(new File(getResources().getPath() + "/sql/update_1.0-1.1.sql"));
+			}
+			catch (Exception e)
+			{
+				throw new ApplicationException(getResources().getI18N().tr("Fehler beim Update der Datenbank"),e);
+			}
+  	}
   }
 
   /**
@@ -151,7 +162,10 @@ public class HBCI extends AbstractPlugin
 
 /**********************************************************************
  * $Log$
- * Revision 1.19  2004-07-04 17:07:59  willuhn
+ * Revision 1.20  2004-07-11 16:14:29  willuhn
+ * @N erster Code fuer Dauerauftraege
+ *
+ * Revision 1.19  2004/07/04 17:07:59  willuhn
  * @B Umsaetze wurden teilweise nicht als bereits vorhanden erkannt und wurden somit doppelt angezeigt
  *
  * Revision 1.18  2004/07/01 19:46:27  willuhn
