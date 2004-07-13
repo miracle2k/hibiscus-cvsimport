f61f33fda907ef98fbd89efbd74e130eace605d8
willuhn <willuhn>
2004-07-13 23:08:37 +0000

    @N Views fuer Dauerauftrag

---
+++ src/de/willuhn/jameica/hbci/gui/views/DauerauftragNeu.java
@@ -0,0 +1,49 @@
+/**********************************************************************
+ * $Source$
+ * $Revision$
+ * $Date$
+ * $Author$
+ * $Locker$
+ * $State$
+ *
+ * Copyright (c) by willuhn.webdesign
+ * All rights reserved
+ *
+ **********************************************************************/
+package de.willuhn.jameica.hbci.gui.views;
+
+import de.willuhn.jameica.gui.views.AbstractView;
+import de.willuhn.util.ApplicationException;
+
+/**
+ */
+public class DauerauftragNeu extends AbstractView
+{
+
+  /**
+   * @see de.willuhn.jameica.gui.views.AbstractView#bind()
+   */
+  public void bind() throws Exception
+  {
+    // TODO Auto-generated method stub
+
+  }
+
+  /**
+   * @see de.willuhn.jameica.gui.views.AbstractView#unbind()
+   */
+  public void unbind() throws ApplicationException
+  {
+    // TODO Auto-generated method stub
+
+  }
+
+}
+
+
+/**********************************************************************
+ * $Log$
+ * Revision 1.1  2004-07-13 23:08:37  willuhn
+ * @N Views fuer Dauerauftrag
+ *
+ **********************************************************************/
