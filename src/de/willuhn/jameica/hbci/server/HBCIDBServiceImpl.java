e8f104a5eead9b3410a852ab54eaf1035ff6a62c
willuhn <willuhn>
2004-07-23 16:23:46 +0000

    *** empty log message ***

--- src/de/willuhn/jameica/hbci/server/HBCIDBServiceImpl.java
+++ src/de/willuhn/jameica/hbci/server/HBCIDBServiceImpl.java
@@ -19,6 +19,7 @@ import de.willuhn.datasource.db.DBServiceImpl;
 import de.willuhn.datasource.rmi.DBService;
 import de.willuhn.jameica.hbci.HBCI;
 import de.willuhn.jameica.plugin.PluginLoader;
+import de.willuhn.jameica.system.Application;
 
 /**
  * @author willuhn
@@ -32,7 +33,9 @@ public class HBCIDBServiceImpl extends DBServiceImpl implements DBService
   public HBCIDBServiceImpl() throws RemoteException
   {
     super("com.mckoi.JDBCDriver",
-          ":jdbc:mckoi:local://" + PluginLoader.getPlugin(HBCI.class).getResources().getWorkPath() + "/db/db.conf?user=hibiscus&password=hibiscus");
+          ":jdbc:mckoi:local://" + PluginLoader.getPlugin(HBCI.class).getResources().getWorkPath() + "/db/db.conf",
+          "hibiscus","hibiscus");
+    this.setClassFinder(Application.getClassLoader().getClassFinder());
   }
 
 }
@@ -40,7 +43,10 @@ public class HBCIDBServiceImpl extends DBServiceImpl implements DBService
 
 /*********************************************************************
  * $Log$
- * Revision 1.1  2004-07-23 15:51:44  willuhn
+ * Revision 1.2  2004-07-23 16:23:46  willuhn
+ * *** empty log message ***
+ *
+ * Revision 1.1  2004/07/23 15:51:44  willuhn
  * @C Rest des Refactorings
  *
  **********************************************************************/
