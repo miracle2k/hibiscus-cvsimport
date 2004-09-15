e48d23ac7dd7675f9b6ae4e43aff5f1fdb7fe429
willuhn <willuhn>
2004-09-15 22:31:42 +0000

    *** empty log message ***

--- src/de/willuhn/jameica/hbci/server/HBCIDBServiceImpl.java
+++ src/de/willuhn/jameica/hbci/server/HBCIDBServiceImpl.java
@@ -19,6 +19,7 @@ import de.willuhn.datasource.db.DBServiceImpl;
 import de.willuhn.datasource.rmi.DBService;
 import de.willuhn.jameica.hbci.HBCI;
 import de.willuhn.jameica.system.Application;
+import de.willuhn.util.I18N;
 
 /**
  * @author willuhn
@@ -37,12 +38,24 @@ public class HBCIDBServiceImpl extends DBServiceImpl implements DBService
     this.setClassFinder(Application.getClassLoader().getClassFinder());
   }
 
+  /**
+   * @see de.willuhn.datasource.Service#getName()
+   */
+  public String getName() throws RemoteException
+  {
+		I18N i18n = Application.getPluginLoader().getPlugin(HBCI.class).getResources().getI18N();
+    return i18n.tr("Datenbank-Service für Hibiscus");
+  }
+
 }
 
 
 /*********************************************************************
  * $Log$
- * Revision 1.4  2004-08-31 18:13:51  willuhn
+ * Revision 1.5  2004-09-15 22:31:42  willuhn
+ * *** empty log message ***
+ *
+ * Revision 1.4  2004/08/31 18:13:51  willuhn
  * *** empty log message ***
  *
  * Revision 1.3  2004/07/25 17:15:06  willuhn
