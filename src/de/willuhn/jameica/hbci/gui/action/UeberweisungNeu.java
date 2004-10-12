2e201a4bffe1bddeb6b138a552fca90dfeb306d4
willuhn <willuhn>
2004-10-12 23:48:39 +0000

    @N Actions

---
+++ src/de/willuhn/jameica/hbci/gui/action/UeberweisungNeu.java
@@ -0,0 +1,41 @@
+/**********************************************************************
+ * $Source$
+ * $Revision$
+ * $Date$
+ * $Author$
+ * $Locker$
+ * $State$
+ *
+ * Copyright (c) by willuhn.webdesign
+ * All rights reserved
+ *
+ **********************************************************************/
+package de.willuhn.jameica.hbci.gui.action;
+
+import de.willuhn.jameica.gui.Action;
+import de.willuhn.jameica.gui.GUI;
+import de.willuhn.util.ApplicationException;
+
+/**
+ * Action fuer neue Ueberweisung.
+ */
+public class UeberweisungNeu implements Action
+{
+
+  /**
+   * @see de.willuhn.jameica.gui.Action#handleAction(java.lang.Object)
+   */
+  public void handleAction(Object context) throws ApplicationException
+  {
+  	GUI.startView(de.willuhn.jameica.hbci.gui.views.UeberweisungNeu.class.getName(),null);
+  }
+
+}
+
+
+/**********************************************************************
+ * $Log$
+ * Revision 1.1  2004-10-12 23:48:39  willuhn
+ * @N Actions
+ *
+ **********************************************************************/
