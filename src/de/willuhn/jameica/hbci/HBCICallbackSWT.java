9552155b79dc654dc5bf6e1fef3c81447cb6f6e2
willuhn <willuhn>
2006-03-28 17:52:23 +0000

    @B bug 218

--- src/de/willuhn/jameica/hbci/HBCICallbackSWT.java
+++ src/de/willuhn/jameica/hbci/HBCICallbackSWT.java
@@ -29,7 +29,6 @@ import org.kapott.hbci.passport.HBCIPassportRDHNew;
 
 import de.willuhn.jameica.gui.GUI;
 import de.willuhn.jameica.hbci.gui.DialogFactory;
-import de.willuhn.jameica.hbci.rmi.Konto;
 import de.willuhn.jameica.hbci.rmi.Nachricht;
 import de.willuhn.jameica.hbci.server.hbci.HBCIFactory;
 import de.willuhn.jameica.security.Wallet;
@@ -49,7 +48,6 @@ public class HBCICallbackSWT extends AbstractHBCICallback
 
 	private I18N i18n;
 	private Hashtable accountCache = new Hashtable();
-  private de.willuhn.jameica.system.Settings settings = new de.willuhn.jameica.system.Settings(this.getClass());
 
   /**
    * ct.
@@ -194,15 +192,11 @@ public class HBCICallbackSWT extends AbstractHBCICallback
         // BUGZILLA 200
         case NEED_PT_SECMECH:
 
-          Konto konto = HBCIFactory.getInstance().getCurrentKonto();
-          if (konto != null)
+          String type = Settings.getSecMech(HBCIFactory.getInstance().getCurrentKonto());
+          if (type != null && type.length() > 0)
           {
-            String type = settings.getString("konto." + konto.getID() + ".secmech",null);
-            if (type != null && type.length() > 0)
-            {
-              retData.replace(0,retData.length(),type);
-              break;
-            }
+            retData.replace(0,retData.length(),type);
+            break;
           }
           
           retData.replace(0,retData.length(),DialogFactory.getPtSechMech(retData.toString()));
@@ -512,7 +506,10 @@ public class HBCICallbackSWT extends AbstractHBCICallback
 
 /**********************************************************************
  * $Log$
- * Revision 1.35  2006-02-23 22:14:58  willuhn
+ * Revision 1.36  2006-03-28 17:52:23  willuhn
+ * @B bug 218
+ *
+ * Revision 1.35  2006/02/23 22:14:58  willuhn
  * @B bug 200 (Speichern der Auswahl)
  *
  * Revision 1.34  2006/02/21 22:51:36  willuhn
