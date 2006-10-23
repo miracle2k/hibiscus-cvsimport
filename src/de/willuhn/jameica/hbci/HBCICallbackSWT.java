f79cdb24824311dc33013bbcdbb2c487fbc82646
willuhn <willuhn>
2006-10-23 10:48:04 +0000

    @B check for pw=null before generating a new password

--- src/de/willuhn/jameica/hbci/HBCICallbackSWT.java
+++ src/de/willuhn/jameica/hbci/HBCICallbackSWT.java
@@ -163,8 +163,10 @@ public class HBCICallbackSWT extends AbstractHBCICallback
           retry = false;
 
           // Das Neuausdenken von Passworten machen wir nur noch
-          // bei PIN/TAN und Chipkarte
-          if (!forceAsk && !isRDH && !isSizRDH)
+          // bei PIN/TAN und Chipkarte. Und auch nur noch dann,
+          // wenn wir kein Passwort gefunden haben. Denn ansonsten
+          // riskieren wir, das Passwort zu ueberschreiben
+          if (pw == null && !forceAsk && !isRDH && !isSizRDH)
           {
             // noch kein Passwort definiert. Dann erzeugen wir
             // ein zufaelliges.
@@ -630,7 +632,10 @@ public class HBCICallbackSWT extends AbstractHBCICallback
 
 /**********************************************************************
  * $Log$
- * Revision 1.45  2006-10-18 16:17:08  willuhn
+ * Revision 1.46  2006-10-23 10:48:04  willuhn
+ * @B check for pw=null before generating a new password
+ *
+ * Revision 1.45  2006/10/18 16:17:08  willuhn
  * @N additional log infos
  *
  * Revision 1.44  2006/10/12 13:07:56  willuhn
