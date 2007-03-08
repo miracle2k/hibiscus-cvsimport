e1335f8c251391b69d8e08f941669c81bbd44dfc
willuhn <willuhn>
2007-03-08 18:56:39 +0000

    @N Mehrere Spalten in Kategorie-Baum

--- src/de/willuhn/jameica/hbci/server/UmsatzTypImpl.java
+++ src/de/willuhn/jameica/hbci/server/UmsatzTypImpl.java
@@ -321,8 +321,12 @@ public class UmsatzTypImpl extends AbstractDBObjectNode implements UmsatzTyp
    */
   public Object getAttribute(String arg0) throws RemoteException
   {
-    if ("umsatz".equals(arg0)) // Synthetisches Attribut "umsatz"
+    if ("umsatz".equals(arg0))
       return new Double(getUmsatz());
+
+    if ("betrag".equals(arg0)) // Synthetisches Attribut "umsatz"/"betrag"
+      return new Double(getUmsatz());
+
     if (arg0 != null && arg0.startsWith("umsatz"))
     {
       // TODO: Beheben! Das ist haesslich!
@@ -420,7 +424,10 @@ public class UmsatzTypImpl extends AbstractDBObjectNode implements UmsatzTyp
 
 /*******************************************************************************
  * $Log$
- * Revision 1.30  2007-03-06 20:06:56  jost
+ * Revision 1.31  2007-03-08 18:56:39  willuhn
+ * @N Mehrere Spalten in Kategorie-Baum
+ *
+ * Revision 1.30  2007/03/06 20:06:56  jost
  * Neu: Umsatz-Kategorien-Übersicht
  * Revision 1.29 2006/12/29 14:28:47 willuhn
  * 
