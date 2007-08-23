c3282e2346cc7c5f171bbb296e03375b61dd543a
willuhn <willuhn>
2007-08-23 12:43:07 +0000

    @C BUGZILLA 275 - Umstellung der Default-Datenbank auf H2

--- src/de/willuhn/jameica/hbci/server/HBCIDBServiceImpl.java
+++ src/de/willuhn/jameica/hbci/server/HBCIDBServiceImpl.java
@@ -43,7 +43,7 @@ public class HBCIDBServiceImpl extends DBServiceImpl implements HBCIDBService
    */
   public HBCIDBServiceImpl() throws RemoteException
   {
-    this(SETTINGS.getString("database.driver",DBSupportMcKoiImpl.class.getName()));
+    this(SETTINGS.getString("database.driver",DBSupportH2Impl.class.getName()));
   }
   
   /**
@@ -236,7 +236,10 @@ public class HBCIDBServiceImpl extends DBServiceImpl implements HBCIDBService
 
 /*********************************************************************
  * $Log$
- * Revision 1.21  2007-07-28 15:51:26  willuhn
+ * Revision 1.22  2007-08-23 12:43:07  willuhn
+ * @C BUGZILLA 275 - Umstellung der Default-Datenbank auf H2
+ *
+ * Revision 1.21  2007/07/28 15:51:26  willuhn
  * @B Bug 447
  *
  * Revision 1.20  2007/07/26 23:55:21  willuhn
