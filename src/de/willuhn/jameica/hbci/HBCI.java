29d831ef93b8b099885c1d5cb510d771bbac6936
willuhn <willuhn>
2007-11-12 00:08:02 +0000

    @N Query-Messages fuer Bankname-Lookup und CRC-Account-Check fuer JVerein

--- src/de/willuhn/jameica/hbci/HBCI.java
+++ src/de/willuhn/jameica/hbci/HBCI.java
@@ -24,6 +24,8 @@ import org.kapott.hbci.callback.HBCICallback;
 import org.kapott.hbci.callback.HBCICallbackConsole;
 import org.kapott.hbci.manager.HBCIUtils;
 
+import de.willuhn.jameica.hbci.messaging.QueryAccountCRCMessageConsumer;
+import de.willuhn.jameica.hbci.messaging.QueryBanknameMessageConsumer;
 import de.willuhn.jameica.hbci.rmi.HBCIDBService;
 import de.willuhn.jameica.hbci.server.HBCIDBServiceImpl;
 import de.willuhn.jameica.plugin.AbstractPlugin;
@@ -103,6 +105,9 @@ public class HBCI extends AbstractPlugin
     File f = new File(path);
     if (!f.exists()) f.mkdirs();
     /////////////////////////////////////////////////////////////////
+
+    Application.getMessagingFactory().getMessagingQueue("hibiscus.query.bankname").registerMessageConsumer(new QueryBanknameMessageConsumer());
+    Application.getMessagingFactory().getMessagingQueue("hibiscus.query.accountcrc").registerMessageConsumer(new QueryAccountCRCMessageConsumer());
 
     initHBCI(getResources().getSettings().getString("hbcicallback.class",HBCICallbackSWT.class.getName()));
     Application.getCallback().getStartupMonitor().addPercentComplete(5);
@@ -301,7 +306,10 @@ public class HBCI extends AbstractPlugin
 
 /**********************************************************************
  * $Log$
- * Revision 1.102  2007-06-21 11:33:13  willuhn
+ * Revision 1.103  2007-11-12 00:08:02  willuhn
+ * @N Query-Messages fuer Bankname-Lookup und CRC-Account-Check fuer JVerein
+ *
+ * Revision 1.102  2007/06/21 11:33:13  willuhn
  * @N PassportRegistry erst bei Bedarf initialisieren
  *
  * Revision 1.101  2007/06/05 00:41:53  willuhn
