6d686ad3be9cd26e40aa2efb73b110a1a2e523fb
willuhn <willuhn>
2008-09-03 11:13:51 +0000

    @N Mehr Suchprovider

--- src/de/willuhn/jameica/hbci/search/LastschriftSearchProvider.java
+++ src/de/willuhn/jameica/hbci/search/LastschriftSearchProvider.java
@@ -15,15 +15,14 @@ package de.willuhn.jameica.hbci.search;
 
 import java.rmi.RemoteException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import de.willuhn.datasource.rmi.DBIterator;
 import de.willuhn.jameica.hbci.HBCI;
-import de.willuhn.jameica.hbci.gui.action.UmsatzDetail;
+import de.willuhn.jameica.hbci.Settings;
+import de.willuhn.jameica.hbci.gui.action.LastschriftNew;
 import de.willuhn.jameica.hbci.rmi.Konto;
-import de.willuhn.jameica.hbci.rmi.Umsatz;
-import de.willuhn.jameica.hbci.server.UmsatzUtil;
+import de.willuhn.jameica.hbci.rmi.Lastschrift;
 import de.willuhn.jameica.search.Result;
 import de.willuhn.jameica.search.SearchProvider;
 import de.willuhn.jameica.system.Application;
@@ -33,16 +32,16 @@ import de.willuhn.util.I18N;
 
 
 /**
- * Implementierung einen Search-Provider fuer die Suche in Umsaetzen.
+ * Implementierung einen Search-Provider fuer die Suche in Lastschriften.
  */
-public class UmsatzSearchProvider implements SearchProvider
+public class LastschriftSearchProvider implements SearchProvider
 {
   /**
    * @see de.willuhn.jameica.search.SearchProvider#getName()
    */
   public String getName()
   {
-    return Application.getPluginLoader().getPlugin(HBCI.class).getResources().getI18N().tr("Umsätze");
+    return Application.getPluginLoader().getPlugin(HBCI.class).getResources().getI18N().tr("Lastschriften");
   }
 
   /**
@@ -53,24 +52,20 @@ public class UmsatzSearchProvider implements SearchProvider
   {
     if (search == null || search.length() == 0)
       return null;
-
+    
     String text = "%" + search.toLowerCase() + "%";
-    DBIterator list = UmsatzUtil.getUmsaetzeBackwards();
+    DBIterator list = Settings.getDBService().createList(Lastschrift.class);
     list.addFilter("LOWER(zweck) LIKE ? OR " +
                    "LOWER(zweck2) LIKE ? OR " +
                    "LOWER(empfaenger_name) LIKE ? OR " +
                    "empfaenger_konto LIKE ? OR " +
-                   "empfaenger_blz LIKE ? OR " +
-                   "LOWER(primanota) LIKE ? OR " +
-                   "LOWER(art) LIKE ? OR " +
-                   "LOWER(customerref) LIKE ? OR " +
-                   "LOWER(kommentar) LIKE ?",
-                   new String[]{text,text,text,text,text,text,text,text,text});
+                   "empfaenger_blz LIKE ?",
+                   new String[]{text,text,text,text,text});
 
     ArrayList results = new ArrayList();
     while (list.hasNext())
     {
-      results.add(new MyResult((Umsatz)list.next()));
+      results.add(new MyResult((Lastschrift)list.next()));
     }
     return results;
   }
@@ -80,15 +75,15 @@ public class UmsatzSearchProvider implements SearchProvider
    */
   private class MyResult implements Result
   {
-    private Umsatz umsatz = null;
+    private Lastschrift l = null;
     
     /**
      * ct.
-     * @param u
+     * @param l
      */
-    private MyResult(Umsatz u)
+    private MyResult(Lastschrift l)
     {
-      this.umsatz = u;
+      this.l = l;
     }
 
     /**
@@ -96,7 +91,7 @@ public class UmsatzSearchProvider implements SearchProvider
      */
     public void execute() throws RemoteException, ApplicationException
     {
-      new UmsatzDetail().handleAction(this.umsatz);
+      new LastschriftNew().handleAction(this.l);
     }
 
     /**
@@ -106,29 +101,16 @@ public class UmsatzSearchProvider implements SearchProvider
     {
       try
       {
+        Konto k = l.getKonto();
+        String[] params = new String[] {
+            k.getLongName(),
+            l.getZweck(),
+            HBCI.DECIMALFORMAT.format(l.getBetrag()),
+            k.getWaehrung(),
+            l.getGegenkontoName()
+           };
         I18N i18n = Application.getPluginLoader().getPlugin(HBCI.class).getResources().getI18N();
-
-        Konto konto           = umsatz.getKonto();
-        Date valuta           = umsatz.getValuta();
-        double betrag         = umsatz.getBetrag();
-        String rel            = i18n.tr(betrag > 0 ? "von" : "an");
-        String zweck          = umsatz.getZweck();
-        String gegenkontoName = umsatz.getGegenkontoName();
-
-        betrag = Math.abs(betrag);
-        if (gegenkontoName == null || gegenkontoName.length() == 0)
-        {
-          return i18n.tr("{0}: {1} {2} - {5}", new String[]{HBCI.DATEFORMAT.format(valuta),
-                                                            HBCI.DECIMALFORMAT.format(betrag), 
-                                                            konto.getWaehrung(),
-                                                            zweck});
-        }
-        return i18n.tr("{0}: {1} {2} {3} {4} - {5}", new String[]{HBCI.DATEFORMAT.format(valuta),
-                                                                  HBCI.DECIMALFORMAT.format(betrag), 
-                                                                  konto.getWaehrung(),
-                                                                  rel,
-                                                                  gegenkontoName,
-                                                                  zweck});
+        return i18n.tr("{0}: ({1}) {2} {3} von {4}",params);
       }
       catch (RemoteException re)
       {
@@ -144,7 +126,7 @@ public class UmsatzSearchProvider implements SearchProvider
 
 /**********************************************************************
  * $Log$
- * Revision 1.2  2008-09-03 11:13:51  willuhn
+ * Revision 1.1  2008-09-03 11:13:51  willuhn
  * @N Mehr Suchprovider
  *
  * Revision 1.1  2008/09/03 00:12:06  willuhn
