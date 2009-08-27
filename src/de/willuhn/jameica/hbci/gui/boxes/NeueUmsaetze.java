b7cc63e75b98c8a693c2a9f17d616b9f7a543923
willuhn <willuhn>
2009-08-27 11:48:09 +0000

    @N Eigenes Konto in Box "Neue Umsaetze" mit anzeigen

--- src/de/willuhn/jameica/hbci/gui/boxes/NeueUmsaetze.java
+++ src/de/willuhn/jameica/hbci/gui/boxes/NeueUmsaetze.java
@@ -23,6 +23,7 @@ import de.willuhn.jameica.hbci.HBCI;
 import de.willuhn.jameica.hbci.Settings;
 import de.willuhn.jameica.hbci.gui.action.UmsatzDetail;
 import de.willuhn.jameica.hbci.gui.parts.UmsatzList;
+import de.willuhn.jameica.hbci.gui.parts.columns.KontoColumn;
 import de.willuhn.jameica.system.Application;
 import de.willuhn.util.I18N;
 
@@ -74,6 +75,7 @@ public class NeueUmsaetze extends AbstractBox
   {
     GenericIterator list = de.willuhn.jameica.hbci.messaging.NeueUmsaetze.getNeueUmsaetze();
     UmsatzList umsaetze = new UmsatzList(list,new UmsatzDetail());
+    umsaetze.addColumn(new KontoColumn("konto_id"));
     umsaetze.setFilterVisible(false);
     umsaetze.paint(parent);
   }
@@ -98,7 +100,10 @@ public class NeueUmsaetze extends AbstractBox
 
 /*********************************************************************
  * $Log$
- * Revision 1.5  2008-03-04 10:36:41  willuhn
+ * Revision 1.6  2009-08-27 11:48:09  willuhn
+ * @N Eigenes Konto in Box "Neue Umsaetze" mit anzeigen
+ *
+ * Revision 1.5  2008/03/04 10:36:41  willuhn
  * @N Default-Hoehe vergroessert
  *
  * Revision 1.4  2007/12/29 18:45:37  willuhn
