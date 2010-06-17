348d6f0cd53b11079bad7bd1cf01ea1669d9e026
willuhn <willuhn>
2010-06-17 11:38:15 +0000

    @C kompletten Code aus "hbci_passport_pintan" in Hibiscus verschoben - es macht eigentlich keinen Sinn mehr, das in separaten Projekten zu fuehren

---
+++ src/de/willuhn/jameica/hbci/passports/pintan/rmi/Passport.java
@@ -0,0 +1,32 @@
+/**********************************************************************
+ * $Source$
+ * $Revision$
+ * $Date$
+ * $Author$
+ * $Locker$
+ * $State$
+ *
+ * Copyright (c) by willuhn.webdesign
+ * All rights reserved
+ *
+ **********************************************************************/
+
+package de.willuhn.jameica.hbci.passports.pintan.rmi;
+
+/**
+ */
+public interface Passport extends de.willuhn.jameica.hbci.passport.Passport
+{
+
+}
+
+
+/*********************************************************************
+ * $Log$
+ * Revision 1.1  2010-06-17 11:38:16  willuhn
+ * @C kompletten Code aus "hbci_passport_pintan" in Hibiscus verschoben - es macht eigentlich keinen Sinn mehr, das in separaten Projekten zu fuehren
+ *
+ * Revision 1.1  2005/08/01 23:28:03  web0
+ * *** empty log message ***
+ *
+ **********************************************************************/
